1874e2bc1c640fb953218f947c7fc132072403b6
mayank-aggarwal28 <mayankaggarwal9628@gmail.com>
2017-07-07 15:16:41 +0530

    First commit

--- app/src/main/java/com/example/mayank/ldqapp/LoginActivity.java
+++ app/src/main/java/com/example/mayank/ldqapp/LoginActivity.java
@@ -74,7 +74,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             // Configure sign-in to request the user's ID, email address, and basic
             // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
             GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestIdToken(getString(R.string.default_web_client))
                     .requestEmail()
                     .build();
             // [END configure_signin]
